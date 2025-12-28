6180b189360e2ca3614590eea1fd358af67191d0
kqye <154298198+kqye@users.noreply.github.com>
2025-12-28 21:27:38 +0800

    Add files via upload

---
+++ SampleFinal/src/samplefinal/SampleFinal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samplefinal;
+
+/**
+ *
+ * @author acer
+ */
+public class SampleFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frm_Sample i = new Frm_Sample();
+        i.setVisible(true);
+    }
+    
+}
